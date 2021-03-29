d0f9c76479a5999b528e63c1546a9c8bb52bb44d
adam-sungmin-park <parksm2@uci.edu>
2021-03-29 21:32:46 +0000

    hashing

--- client/app.jsx
+++ client/app.jsx
@@ -1,8 +1,52 @@
 import React from 'react';
 import Home from './pages/home';
+import parseRoute from './pages/parseroute';
 
 export default class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      route: parseRoute(window.location.hash),
+      userId: "",
+      email:"",
+      password:''
+    }
+  }
+  componentDidMount(){
+    window.addEventListener('hashchange',(event)=>{
+      this.setState({
+        route:parseRoute(window.location.hash)
+      })
+    })
+  }
+  updateHash(hash){
+    this.setState({
+      route:hash
+    })
+    this.location.reload()
+  }
+  renderPage(){
+    const { route } = this.state
+    if(route.path === ""){
+      return <LandingPage/>
+    }
+    if(route.path === "home"){
+      return <Home/>
+    }
+
+  }
+
+
+
   render() {
-    return <Home />;
+    const {route} = this.state
+    if (route.path !== "") {
+      return (
+        <div id="container">
+          <NavBar userId={this.state.userId}></NavBar>
+          {this.renderPage()}
+        </div>
+      )
+    }
   }
 }
