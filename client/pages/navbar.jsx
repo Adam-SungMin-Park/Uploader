2edd90acb4fd8163201780fd9741b2fded31e430
adam-sungmin-park <parksm2@uci.edu>
2021-04-08 07:23:24 +0000

    navbar

--- client/pages/navbar.jsx
+++ client/pages/navbar.jsx
@@ -11,7 +11,31 @@ export default class NavBar extends React.Component  {
   render(){
     return(
       <div id = "navBarContainer">
-        <a className = "tab" href = "#home"><span className = "material-icons md-60">fitness_center</span></a>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">Inventory</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#home">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#orders">Orders</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#setting">Setting</a>
+                </li>
+              </ul>
+              <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
+                <button class="btn btn-outline-success" type="submit">Search</button>
+              </form>
+            </div>
+          </div>
+        </nav>
       </div>
     )
   }
